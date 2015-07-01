4d4b9a92bbda90be3e24b576c8716de7e1d26206
torresal <alexander.p.torres@jpl.nasa.gov>
2015-07-01 15:26:32 -0700

    update-1 to crawler

--- crawler_at2.py
+++ crawler_at2.py
@@ -3,9 +3,10 @@ __author__ = 'torresal'
     # path = “{starting_path}/{type}/{identifier}.json”
     # ingest.py {starting path} is Dexters script
     # Loop statement : for iterating_var in sequence:
-import os,re
 
-stpath = "/Users/torresal/2015-06-22_v0/articles"
+import os,re, subprocess
+
+stpath = "/Users/torresal/2015-06-22_v0"
 
 for (root,dirs,files) in os.walk(stpath):
       files.sort()
@@ -16,8 +17,9 @@ for (root,dirs,files) in os.walk(stpath):
         if match:
             t, id = match.groups()
             #print ('match',t, id)
-            cmd ="echo 'ingest_{}.py {}_{}.json'" .format(t, t, id)
-            os.system(cmd)
+            i ="ingest_{}.py {}_{}.json" .format(t, t, id)
+            subprocess.call('python /Users/torresal/{}' .format(i), shell=True)
+            #print('python /Users/torresal/{}' .format(i))
 
 
 
