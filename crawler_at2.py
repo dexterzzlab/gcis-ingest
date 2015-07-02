c3e5fb118ee1702e3de5b8734a85ff3857080316
torresal <alexander.p.torres@jpl.nasa.gov>
2015-07-02 09:28:51 -0700

    updated 7/2/15

--- crawler_at2.py
+++ crawler_at2.py
@@ -7,11 +7,13 @@ __author__ = 'torresal'
 import os,re, subprocess
 
 stpath = "/Users/torresal/2015-06-22_v0"
+#insert path to file#
 
 for (root,dirs,files) in os.walk(stpath):
       files.sort()
       for file in files:
         dir_path = os.path.join(root, file)
+        #extracts the root, dirs, files from iven path#
         #print(dir_path)
         match =  re.search(r'.*/(.*?)_(.*?)\.json$', dir_path)
         if match:
