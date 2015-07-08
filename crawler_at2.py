811e3fe152342b5b8bb6a35f9d663f6784f0e531
Ubuntu <ubuntu@ip-172-31-47-25.ec2.internal>
2015-07-08 15:57:48 +0000

    Updated 7/08/15

--- crawler_at2.py
+++ crawler_at2.py
@@ -1,12 +1,8 @@
 __author__ = 'torresal'
-# Personal notes
-    # path = “{starting_path}/{type}/{identifier}.json”
-    # ingest.py {starting path} is Dexters script
-    # Loop statement : for iterating_var in sequence:
 
 import os,re, subprocess
-
-stpath = "/Users/torresal/2015-06-22_v0"
+#/home/ubuntu/dump_ingest/2015-07-02_v0/reports
+stpath = "/home/ubuntu/dump_ingest/2015-07-02_v0"
 #insert path to file#
 
 for (root,dirs,files) in os.walk(stpath):
@@ -19,10 +15,9 @@ for (root,dirs,files) in os.walk(stpath):
         if match:
             t, id = match.groups()
             #print ('match',t, id)
-            i ="ingest_{}.py {}_{}.json" .format(t, t, id)
-            subprocess.call('python /Users/torresal/{}' .format(i), shell=True)
-            #print('python /Users/torresal/{}' .format(i))
-
+            i ="{}_ingest.py {}_{}.json" .format(t, t, id)
+            subprocess.call('python /home/ubuntu/dump_ingest/ingest_scripts/{}/{}' .format(t,i), shell=True)
+            #print('python /home/ubuntu/dump_ingest/ingest_scripts/report/{}' .format(i))
 
 
 
